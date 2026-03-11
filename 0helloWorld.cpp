063b93013c6a01a0215e843841c7e6712613d4a4
adii79 <adityashigam9755@gmail.com>
2026-03-11 23:27:02 +0530

    first commit

---
+++ 0helloWorld.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main(){
+
+    // this is comments 
+    /*
+    this is a multi line commnet.
+    */
+
+    std::cout<<"I like the modaks"<<std::endl;
+    std::cout<<"with the ghee"<<" \n";
+    std::cout<<":)"<<" \n";
+    return 0;
+}
